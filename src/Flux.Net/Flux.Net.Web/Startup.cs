2b01708c803db7418baa419381ea2726f185dd50
Cedric Dumont <cedric.dumont.ext@atos.net>
2015-12-31 12:01:42 +0100

    added web project with signalR

---
+++ src/Flux.Net/Flux.Net.Web/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Flux.Net.Web.Startup))]
+
+namespace Flux.Net.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
